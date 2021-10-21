fc6aa30118f5aaa430ebe7a0b5baeb565b739ed5
sohamrajput7 <sohamrajput7@gmail.com>
2021-10-22 04:25:55 +0530

    updated URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost:27017/contactdb"
+    //"URI": "mongodb://localhost:27017/contactdb"
+    "URI": "mongodb+srv://soham:0pAzfF64ldXlUnXy@mongodbserver.j8p8a.mongodb.net/contactdb?retryWrites=true&w=majority"
 }
